ba7d2500eb31a021fb2037113b8a8b1617c99dff
Mask13 <aaron.r.samuel@gmail.com>
2025-10-09 23:21:33 -0400

    create the main python file

---
+++ main.py
@@ -0,0 +1,9 @@
+#Author: Aaron Samuel
+#Date: 10/9/2025
+
+#Starts the calculator application by creating an instance of the CalculatorUI class.
+
+from app.calculatorUI import CalculatorUI
+
+if __name__ == "__main__":
+    CalculatorUI()
